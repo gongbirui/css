2fe384860645402bfece2025f04661faf3153a92
gongbirui <37900913+gongbirui@users.noreply.github.com>
2021-09-17 00:32:17 +0800

    解决部分已知问题
    待解决：双斜杠注释问题

--- m.js
+++ m.js
@@ -1,12 +1,10 @@
 const parse = require("./lib/cssCopy/parser");
 const stringify = require("./lib/cssCopy/stringify");
 let css = `
-  list-style: none;
-  li {
-    list-style-image: none;
-    list-style-type: none;
-    margin-left: 0px;
-  }
+body {
+  color: #333; // 这种注释内容不会出现在生成的css文件中
+  padding: 0; /* 这种注释内容会出现在生成的css文件中 */
+}
 `;
 let ast = parse(css);
 
